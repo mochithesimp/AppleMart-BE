3cd791c2bb3e4299cf43f0d8bd3444d014237357
lap03 <tinle7347@gmail.com>
2025-02-21 18:19:59 +0700

    add refreshToken

--- iPhoneBE.API/iPhoneBE.Service/Interfaces/IAccountServices.cs
+++ iPhoneBE.API/iPhoneBE.Service/Interfaces/IAccountServices.cs
@@ -1,11 +1,15 @@
 ﻿using iPhoneBE.Data.Models.AuthenticationModel;
+using iPhoneBE.Data.ViewModels.AuthenticationDTO;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 
 namespace iPhoneBE.Service.Interfaces
 {
     public interface IAccountServices
     {
-        Task<string> LoginAsync(LoginModel model);
+        Task<JwtViewModel> LoginAsync(LoginModel model);
         Task<IdentityResult> RegisterAsync(RegisterModel model);
+        Task<JwtViewModel> ValidateRefreshToken(RefreshTokenModel model);
+        Task ValidateToken(TokenValidatedContext context);
     }
 }
---
+++ iPhoneBE.API/iPhoneBE.Service/Services/UserServices.cs
@@ -0,0 +1,28 @@
+﻿using iPhoneBE.Data.Model;
+using iPhoneBE.Service.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPhoneBE.Service.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserServices(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<User> FindByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+    }
+}
