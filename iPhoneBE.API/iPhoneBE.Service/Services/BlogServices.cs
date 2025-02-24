24c2247ed408aa970d711416c6bd9283bf584d6a
lap03 <tinle7347@gmail.com>
2025-02-25 00:45:41 +0700

    add category and user api

---
+++ iPhoneBE.API/iPhoneBE.Service/Services/BlogServices.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using iPhoneBE.Data.Entities;
+using iPhoneBE.Data.Interfaces;
+using iPhoneBE.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPhoneBE.Service.Services
+{
+    public class BlogServices : IBlogServices
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public BlogServices(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Blog>> GetAllAsync()
+        {
+            var blogs = await _unitOfWork.BlogRepository.GetAllAsync(b => !b.IsDeleted, b => b.BlogImages);
+            return blogs;
+        }
+    }
+}
